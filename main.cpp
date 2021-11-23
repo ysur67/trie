d73c79b638c0bcd4a6fca26d3a3751a16e01e36c
ysur67 <rus.muratow@gmail.com>
2021-11-23 11:28:00 +0300

    fix: trie impl

--- main.cpp
+++ main.cpp
@@ -8,42 +8,41 @@ using std::map;
 class Node
 {
 public:
-    Node(char* value = NULL){
-        this->value = value;
-    };
-    char* value;
+    Node(){ };
     map<char, Node*> nodes;
     bool isEnd;
 };
 
-Node* getNodeTree(Node* node, const char *letter)
+void getNodeTree(Node* root, const char *letter)
 {
     if (letter == NULL || *letter == '\0') {
-        return node;
-    }
-    if (node->nodes.count(*letter) < 1) {
-        node->nodes.insert(std::pair<char, Node*>(*letter, new Node((char*)letter)));
-    }
-    letter++;
-    return getNodeTree(node, letter);
-}
-
-void printNode(Node* node) {
-    if (node == NULL || node->nodes.size() == 0) {
         return;
     }
-    cout << node->value << std::endl;
-    for(const auto s : node->nodes) {
-        cout << (char) s.first << std::endl;
-        printNode(s.second);
+    Node* currentNode = root;
+    if (root->nodes.count(*letter) < 1) {
+        currentNode = new Node();
+        root->nodes.insert(std::pair<char, Node*>(*letter, currentNode));
+    }
+    return getNodeTree(currentNode, ++letter);
+}
+
+void printNode(Node* root) {
+    if (root == NULL || root->nodes.size() == 0) {
+        return;
+    }
+    for(const auto node : root->nodes) {
+        cout << (char) node.first << std::endl;
+        printNode(node.second);
     }
 }
 
 int main()
 {
     string word = "asdf";
+    string word2 = "basf";
     Node root = Node();
-    Node* node = getNodeTree(&root, word.c_str());
-    printNode(node);
+    getNodeTree(&root, word.c_str());
+    getNodeTree(&root, word2.c_str());
+    printNode(&root);
     return 0;
 }
